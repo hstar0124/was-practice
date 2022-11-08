684a0b305c5930e3f54e1d8591654c234f1e9756
hstar0124 <dunjun10@naver.com>
2022-11-08 16:57:37 +0900

    Initial commit

---
+++ src/main/java/com/example/calculator/NewArithmeticOperator.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+
+public interface NewArithmeticOperator {
+
+    boolean supports(String operator);
+    int calculate(PositiveNumber operand1, PositiveNumber operand2);
+
+}
