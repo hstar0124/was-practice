62a004472becf2bb08c872bc363cbc6bbc35dd4d
hstar0124 <dunjun10@naver.com>
2022-11-09 09:44:35 +0900

    [UPDATE] Main Thread로 처리하던 작업을 Multi Thread가 가능하도록 변경

--- src/main/java/com/example/CustemWebApplicationServer.java
+++ src/main/java/com/example/CustemWebApplicationServer.java
@@ -30,37 +30,7 @@ public class CustemWebApplicationServer {
             while ((clientSocker = serverSocket.accept()) != null) {
                 logger.info("[CustemWebApplicationServer] client connected!");
 
-
-                // Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다
-                try (InputStream in = clientSocker.getInputStream(); OutputStream out = clientSocker.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-//                    String line;
-//                    while((line = br.readLine()) != ""){
-//                        System.out.println(line);
-//                    }
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-
-                    }
-
-                }
+                new Thread(new ClientRequestHandler(clientSocker)).start();
 
             }
 
